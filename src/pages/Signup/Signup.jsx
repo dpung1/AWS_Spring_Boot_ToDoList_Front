56f3b263f9a23881ac5992883004c1a4853859c8
dpung1 <pung_12@naver.com>
2023-10-10 21:21:30 +0900

    feat: 회원가입 -ing

---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import * as S from "./Style"
+
+
+function Signup(props) {
+    const [ signupUser, setSingupUser ] = useState({
+        email: "",
+        password: "",
+        name: ""
+    })
+    
+    const SignupInputOnchange = (e) => {
+        const { name, value } = e.target;
+    
+        setSingupUser({
+            ...signupUser,
+            [name]: value
+        })
+    }
+    
+    const SingupSubmitOnClick = async () => {
+        try {
+        await axios.post("http://localhost:8080/auth/signup", signupUser);
+        alert("회원가입 성공!!")
+        } catch(error) {
+        const responseErrors = error.response.data;
+        const keys = Object.keys(responseErrors);
+    
+        if(keys.includes("email")) {
+            alert(responseErrors.email);
+        } else if(keys.includes("password")) {
+            alert(responseErrors.password);
+        } else if(keys.includes("name")) {
+            alert(responseErrors.name)
+        } else {
+            alert("회원가입 실패!!")
+        }
+        }
+    }
+    return (
+    <>
+        <div css={S.SContainer}>
+            <h1 css={S.SHeader}>회원가입</h1>
+            <div css={S.SInputBox}>
+                <input type='text' name='email' placeholder='이메일' onChange={SignupInputOnchange}/>
+            </div>
+            <div css={S.SInputBox}>
+                <input type='password' name='password' placeholder='비밀번호' onChange={SignupInputOnchange}/>
+            </div>
+            <div css={S.SInputBox}>
+                <input type='text' name='name' placeholder='이름' onChange={SignupInputOnchange}/>
+            </div>
+            <div css={S.SButtonBox}>
+                <button onClick={SingupSubmitOnClick}>가입하기</button>
+            </div>
+        </div>
+    </>
+    );
+}
+
+export default Signup;
---
+++ src/pages/Signup/Style.js
@@ -0,0 +1,26 @@
+import { css } from "@emotion/react";
+
+export const SContainer = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    margin: 50px auto;
+    border: 1px solid #dbdbdb;
+    width: 650px;
+    height: 850px;
+`;
+
+export const SHeader = css`
+    font-size: 25px;
+    padding: 5px;
+`;
+
+export const SInputBox = css`
+    padding: 5px;
+`;
+
+export const SButtonBox = css`
+    padding: 5px;
+`;
